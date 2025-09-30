e21a1629c86cc6ad7f070379a79054163cb2ef5d
SonicDRJ <jhv9096@rit.edu>
2025-09-30 19:44:11 -0400

    Project Initialization
    Creating the SpringBoot project with Spring Initilizr and IntelliJ.

---
+++ src/main/java/com/jhv9096springboot/task_manager_practice/TaskManagerPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.jhv9096springboot.task_manager_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhv9096springboot/task_manager_practice/TaskManagerPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhv9096springboot.task_manager_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
