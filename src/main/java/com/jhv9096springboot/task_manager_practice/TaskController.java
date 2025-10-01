f038ec982c2bce3976f51bd7384abdd4924dc118
SonicDRJ <jhv9096@rit.edu>
2025-10-01 11:33:38 -0400

    Created TaskController
    Created task controller for handling the API endpoints for the task manager.

---
+++ src/main/java/com/jhv9096springboot/task_manager_practice/TaskController.java
@@ -0,0 +1,42 @@
+package com.jhv9096springboot.task_manager_practice;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    private final TaskRepository repository;
+
+    public TaskController(TaskRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Task not found"));
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return repository.save(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task task) {
+        return repository.update(id, task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        repository.delete(id);
+    }
+
+}
