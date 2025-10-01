f16da29cd8e810a78606c701ddb219df5e08f8cb
SonicDRJ <jhv9096@rit.edu>
2025-10-01 11:15:41 -0400

    Created TaskRepository
    Created the task repository class for storing the tasks in the task manager.

---
+++ src/main/java/com/jhv9096springboot/task_manager_practice/TaskRepository.java
@@ -0,0 +1,35 @@
+package com.jhv9096springboot.task_manager_practice;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+public class TaskRepository {
+    private final Map<Long, Task> tasks = new HashMap<>();
+    private Long nextId = 1L;
+
+    public List<Task> findAll() {
+        return new ArrayList<>(tasks.values());
+    }
+
+    public Optional<Task> findById(Long id) {
+        return Optional.ofNullable(tasks.get(id));
+    }
+
+    public Task save(Task task) {
+        task.setId(nextId++);
+        tasks.put(task.getId(), task);
+        return task;
+    }
+
+    public void delete(Long id) {
+        tasks.remove(id);
+    }
+
+    public Task update(Long id, Task updatedTask) {
+        updatedTask.setId(id);
+        tasks.put(id, updatedTask);
+        return updatedTask;
+    }
+
+}
